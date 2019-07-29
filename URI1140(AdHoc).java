2f7bff9648fb1916f6f2a8bf3bdd5f8d23f155c6
Tristan Dufault <32806910+dufaultt@users.noreply.github.com>
2019-07-29 00:48:34 -0400

    Update URI1140(AdHoc).java

--- URI1140(AdHoc).java
+++ URI1140(AdHoc).java
@@ -1,4 +1,3 @@
-//Work in Progress
 import java.util.Scanner;
 import java.io.BufferedInputStream;
 
@@ -16,20 +15,40 @@ public class Main
 		while(true)
 		{
 			sentence = inp.nextLine();
+			flag = true;
 			
 			if(sentence.equals("*"))
 			{
+
 				break;
 			}
 			String[] Words = sentence.split(" ");
-			String[] Letters;
+			char Letter1, Letter2;
 			
-			for(int i = 0; i<Words.length; i++)
+			Letter1 = Words[0].charAt(0);
+			Letter1 = Character.toLowerCase(Letter1);
+			
+			for(int i = 1; i<Words.length; i++)
 			{
-				Letters = Words[i];
+				Letter2 = Words[i].charAt(0);
+				Letter2 = Character.toLowerCase(Letter2);
+				if(Letter2 != Letter1)
+				{
+					flag = false;
+				}
+				
 			}
 			
-			System.out.println(sentence);
+			if(flag == true)
+			{
+				System.out.println("Y");
+			}
+			else
+			{
+				System.out.println("N");
+			}
+			
+
 		}
 	}
 }
